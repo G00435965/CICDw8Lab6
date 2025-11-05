6c7e9e08c7b5c4cd97b421cf04994025d6de0a3a
KEELANMURPHY-STUDENT <G00435965@atu.ie>
2025-11-05 14:13:16 +0000

    created project

---
+++ src/main/java/ie/atu/week7cicd/Week7CicdApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.week7cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week7CicdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week7CicdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/week7cicd/Week7CicdApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week7cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week7CicdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
